5be939031794016c9ea903992409936bbe06cad0
Jo Ey Chong <joeychong@Jos-MacBook-Pro.localhost>
2019-02-17 22:29:21 -0800

    Second

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Jo Ey Chong on 2/17/19.
+//  Copyright © 2019 Jo Ey. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
